e58c3aef2c525489b1faab0267540e83a28282f1
HAYASHI65 <hayashim853@gmail.com>
2021-09-02 19:02:41 +0900

    ログイン状態でのトップページ表示切り替えの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
   validates :birthday, presence: true
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
     validates :last_name
     validates :first_name
   end
 
-  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/} do
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/ } do
     validates :last_name_kana
     validates :first_name_kana
   end
