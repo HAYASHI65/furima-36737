db1b7d3da0fe5d00411406bae95a5b5c8cbd1896
HAYASHI65 <hayashim853@gmail.com>
2021-09-03 19:30:30 +0900

    Active_hashの準備・実装

---
+++ app/models/item.rb
@@ -0,0 +1,10 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :fee
+  belongs_to :prefecture
+  belongs_to :delivery_days
+
+  validates :category_id, numericality:{ other_than: 1, message: "cant't be blank"}
+end
