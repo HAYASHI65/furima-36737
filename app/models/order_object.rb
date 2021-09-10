2cba490b303728e36604cdd6954fb11fd15f0e6e
HAYASHI65 <hayashim853@gmail.com>
2021-09-10 09:01:50 +0900

    テストコードの実装

--- app/models/order_object.rb
+++ app/models/order_object.rb
@@ -5,11 +5,11 @@ class OrderObject
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :block
-    validates :phone_number, format: { with: /\A[0-9]{10,11}+\z/, message: 'is invalid. Input half-width characters' }
+    validates :phone_number, format: { with: /\A[0-9]{10,11}+\z/, message: 'is invalid. Input only number' }
     validates :token
   end
 
